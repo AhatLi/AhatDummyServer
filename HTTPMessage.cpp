ecc752fe29083d2f6045d377d4e8638a88177cb9
ahatLi <ahatahat123@gmail.com>
2019-11-14 15:46:43 +0900

    파이썬 연동 및 자체 스크립트를 위한 코드 추가

---
+++ HTTPMessage.cpp
@@ -0,0 +1,124 @@
+#include "HTTPMessage.h"
+
+HTTPMessage::HTTPMessage()
+{
+	header_code = "200";
+	header_title = "";
+	header_contentType = "";
+	header_contentLength = 0;
+
+    body_type = "raw";
+    body_file = "";
+    body_function = "";
+    body_function_param_num = 0;
+    body_function_param = "";
+    body_text = "";
+}
+
+void HTTPMessage::setHeaderCode(std::string value)
+{
+	header_code = value;
+}
+
+void HTTPMessage::setHeaderTitle(std::string value)
+{
+	header_title = value;
+}
+
+void HTTPMessage::setHeaderContentType(std::string value)
+{
+	header_contentType = value;
+}
+
+void HTTPMessage::setHeaderContentLength(int value)
+{
+	body_file = value;
+}
+
+void HTTPMessage::setBodyType(std::string value)
+{
+	body_type = value;
+}
+
+void HTTPMessage::setBodyFile(std::string value)
+{
+	body_file = value;
+}
+
+void HTTPMessage::setBodyFunction(std::string value)
+{
+	body_function = value;
+}
+
+void HTTPMessage::setBodyFunctionParamNum(int value)
+{
+	body_function_param_num = value;
+}
+
+void HTTPMessage::setBodyFunctionParam(std::string value)
+{
+	body_function_param = value;
+}
+
+void HTTPMessage::addBodyText(std::string value)
+{
+	if(!body_text.empty())
+	{
+		body_text += "\n";
+	}
+	body_text += value;
+}
+
+std::string HTTPMessage::getMessage()
+{
+	if(body_type.compare("python3.8") == 0)
+	{
+
+	}
+	else if(body_type.compare("python3.7") == 0)
+	{
+
+	}
+	else
+	{
+		/* raw */
+	}
+	
+
+
+
+
+
+
+
+	std::string message = "HTTP/1.1 ";
+	message += header_code;
+
+	if(!header_title.empty())
+	{
+		message += " ";
+		message += header_title;
+	}
+	else if(!header_code.compare("200"))
+	{
+		header_title = " OK";
+	}
+	else if(!header_code.compare("400"))
+	{
+		header_title = " Bad Request";
+	}
+
+	message += "\r\nAccept: *\r\nConnection: close";
+	if(header_contentType.compare("") != 0)
+	{
+		message += "\r\nContent-Type: ";
+		message += header_contentType;
+	}
+	message += "\r\nContent-Length: ";
+	message += std::to_string(body_text.length());
+	message += "\r\n\r\n";
+
+	message += body_text;
+
+	return message;
+}
---
+++ HTTPMessage.h
@@ -0,0 +1,38 @@
+#ifndef __AHAT_HTTPMESSAGE_H__
+#define __AHAT_HTTPMESSAGE_H__
+
+#include <iostream>
+
+class HTTPMessage
+{
+private:
+    std::string header_code;
+    std::string header_title;
+    std::string header_contentType;
+    int header_contentLength;
+    
+    std::string body_type;
+    std::string body_file;
+    std::string body_function;
+    int body_function_param_num;
+    std::string body_function_param;
+
+    std::string body_text;
+public:
+    HTTPMessage();
+	void setHeaderCode(std::string value);
+	void setHeaderTitle(std::string value);
+	void setHeaderContentType(std::string value);
+	void setHeaderContentLength(int value);
+
+	void setBodyType(std::string value);
+	void setBodyFile(std::string value);
+	void setBodyFunction(std::string value);
+	void setBodyFunctionParamNum(int value);
+	void setBodyFunctionParam(std::string value);
+	void addBodyText(std::string value);
+
+	std::string getMessage();
+};
+
+#endif
